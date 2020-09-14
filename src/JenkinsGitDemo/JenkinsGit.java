078cc0abe30d874829154beb4922a9aa60de934e
Dada <praveen@practice.com>
2020-09-14 18:30:26 +0530

    Java Jenkis GIT Demo

---
+++ src/JenkinsGitDemo/JenkinsGit.java
@@ -0,0 +1,9 @@
+package JenkinsGitDemo;
+
+public class JenkinsGit {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Hello world ......" + i);
+		}
+	}
+}
